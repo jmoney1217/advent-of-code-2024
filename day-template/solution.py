47fc74a4ecf513f49107c3563dc675fef7793beb
Jared Suttles <jared.suttles@gmail.com>
2024-12-01 01:08:42 -0600

    updates day 1

--- day-template/solution.py
+++ day-template/solution.py
@@ -1,5 +1,30 @@
-infile = "sample_in.txt"
-infile = "input.txt"
+import re
 
-with open(infile, 'r') as ifp:
-    pass
+def readinput():
+    infile = "sample_in.txt"
+    # infile = "input.txt"
+    with open(infile, 'r') as ifp:
+        lines = [ line.strip() for line in ifp.readlines() ]
+    return lines        
+
+def part1(lines):
+    answer = 0
+    for line in lines:
+        pass
+    print("---")
+    print(answer)
+
+def part2(lines):
+    answer = 0
+    for line in lines:
+        pass
+    print("---")
+    print(answer)
+
+def main():
+    lines = readinput()
+    part1(lines)
+    part2(lines)
+
+if __name__=="__main__":
+    main()
